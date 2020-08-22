930c51eefefd5e2636f7bed1abcc9ce9d1d8f69b
Piotr Michna <pm@piotrmichna.pl>
2020-08-22 13:40:06 +0200

    utworzenie glownego pliku projektu main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * avr_pompy.c
+ *
+ * Created: 22.08.2020 13:24:02
+ * Author : Piotr Michna
+ * e-mail : pm@piotrmichna.pl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
