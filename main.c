36b75603f52e66c2ec96a0eb6eae3ec5203c09e6
Piotr Michna <pm@piotrmichna.pl>
2020-08-22 14:18:34 +0200

    definicjie pinow podstawowych

--- main.c
+++ main.c
@@ -14,6 +14,28 @@
 
 #include "macr.h"
 
+// SYSTEMOWE
+#define DET_INT_OFF 1	//detekcja przejscia przez zero napiêcia sieciowego
+#define PWR_OFF 0		//sterowanie zasilaczem dla peryferi
+#define LED_OFF 0		//dioda led pomocnicza
+#define TXEN_OFF 1		//sterowanie kierunkiem komunikacji RS485
+#define TX_OFF 0		//sterowanie lini¹ TX
+#define RX_OFF 0		//sterowanie linia RX
+
+#define DET_INT_PIN PD3
+#define PWR_PIN PC4
+#define LED_PIN PC5
+#define TXEN_PIN PD2
+#define TX_PIN PD1
+#define RX_PIN PD0
+
+#define DET_INT_PORT D
+#define PWR_PORT C
+#define LED_PORT C
+#define TXEN_PORT D
+#define TX_PORT D
+#define RX_PORT D
+
 int main(void)
 {
     
