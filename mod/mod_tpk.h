1200121986c2e5a57a5c58c859b11df84d350843
Piotr Michna <pm@piotrmichna.pl>
2020-08-23 06:45:33 +0200

    utworzenie pliku naglowkowego biblioteki modulu sterowania
    sterowanie przekaznikiem oraz triakiem

---
+++ mod/mod_tpk.h
@@ -0,0 +1,17 @@
+/*
+ * mod_tpk.h
+ *
+ * Created: 23.08.2020 06:41:43
+  * Author : Piotr Michna
+  * e-mail : pm@piotrmichna.pl
+ */ 
+
+
+#ifndef MOD_TPK_H_
+#define MOD_TPK_H_
+
+
+
+
+
+#endif /* MOD_TPK_H_ */
