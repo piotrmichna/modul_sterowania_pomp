382bfdedd86ca9383586b765712ca2e2e6b034df
Piotr Michna <pm@piotrmichna.pl>
2020-08-24 12:25:26 +0200

    definicje buforow nadawczego i odbiorczego komunikacji UART

--- uart/uart328pb.c
+++ uart/uart328pb.c
@@ -8,3 +8,13 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdlib.h>
+
+#include "uart328pb.h"
+
+volatile char UART_RxBuf[UART_RX_BUF_SIZE];		// definicja bufora odbiorczego
+volatile uint8_t UART_RxHead;					// indeks poczatkowy
+volatile uint8_t UART_RxTail;					// indeks koncowy
+
+volatile char UART_TxBuf[UART_TX_BUF_SIZE];		// definicja bufora nadawczego
+volatile uint8_t UART_TxHead;					// indeks poczatkowy
+volatile uint8_t UART_TxTail;					// indeks koncowy
