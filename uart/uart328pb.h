71fcc77cde94786f846e241461c9c383e1ddf170
Piotr Michna <pm@piotrmichna.pl>
2020-08-24 12:10:33 +0200

    utworzenie pliku naglowkowego oraz definicji biblioteki UART

---
+++ uart/uart328pb.h
@@ -0,0 +1,21 @@
+/*
+ * uart328pb.h
+ *
+ * Author : Piotr Michna
+ * e-mail : pm@piotrmichna.pl
+ */ 
+
+
+#ifndef UART328PB_H_
+#define UART328PB_H_
+
+#define UART_BAUD 115200									// predkosc
+#define __UBRR ((F_CPU+UART_BAUD*8UL) / (16UL*UART_BAUD)-1) // UBRR dla U2X=0
+
+#define UART_RX_BUF_SIZE 16									// rozmiar bufora odbiorczego
+#define UART_RX_BUF_MASK ( UART_RX_BUF_SIZE - 1)			// maska bufora
+
+#define UART_TX_BUF_SIZE 16									// rozmiar bufora nadawczego
+#define UART_TX_BUF_MASK ( UART_TX_BUF_SIZE - 1)			// maska bufora
+
+#endif /* UART328PB_H_ */
