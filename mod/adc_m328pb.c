ffb69c436617dbf3d1c466233a40a57fdf10cb1f
Piotr Michna <pm@piotrmichna.pl>
2020-08-25 00:18:46 +0200

    definicja funkcji inicjujacej przetwornika ADC

--- mod/adc_m328pb.c
+++ mod/adc_m328pb.c
@@ -12,3 +12,9 @@
 #include <avr/sleep.h>
 
 #include "adc_m328pb.h"
+
+void adc_init(void){
+	ADMUX=ADC_REFS;					//wybranie napiecia odniesienia
+	ADCSRA=(1<<ADEN) | (1<<ADIE);	//wlaczenie przetwornika i zezwolenie naprzerwanie
+	ADCSRA |= ADC_REFS;				//ustawienie preskalera
+}
