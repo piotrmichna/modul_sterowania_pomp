409ec23599bef77ad4bf73d27b1ef5a5ba0ecc8e
Piotr Michna <pm@piotrmichna.pl>
2020-08-24 12:42:23 +0200

    procedura obslugi przerwania od lini RXD

--- uart/uart328pb.c
+++ uart/uart328pb.c
@@ -26,3 +26,17 @@ void USART_Init( uint16_t baud ) {
 	UCSR0B |= (1<<RXCIE0);						// zezwolenie na przerwanie od odbiornika
 	UCSR0C = (3<<UCSZ00);						// ramka 8bit 1bit stop
 }
+
+ISR( USART0_RX_vect ) {
+	uint8_t tmp_head;
+	char data;
+
+	data = UDR0; //odczyt bufora sprzetowego
+	tmp_head = ( UART_RxHead + 1) & UART_RX_BUF_MASK;	// inkremetacja licznika
+	if ( tmp_head == UART_RxTail ) {
+		// przepelnieie bufora
+		} else {
+		UART_RxHead = tmp_head; 		// zapis indeksu
+		UART_RxBuf[tmp_head] = data; 	// zapis do bufora odbiorczego
+	}
+}
