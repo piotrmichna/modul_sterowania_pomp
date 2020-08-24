8c6bf15e21e1bbc0ebe51f0253be1a6cca68dc60
Piotr Michna <pm@piotrmichna.pl>
2020-08-24 23:36:08 +0200

    podstawowe makrodefinicje kofiguracji przetwornika ADC

--- mod/adc_m328pb.h
+++ mod/adc_m328pb.h
@@ -10,7 +10,17 @@
 #ifndef ADC_M328PB_H_
 #define ADC_M328PB_H_
 
+//#define ADC_REFS 0		// AREF, zewnêtrzne napiecie odniesienia
+//#define ADC_REFS 64		// AVcc
+#define ADC_REFS 192	// Internal 1.1V
 
+//#define ADC_PRESCALER 1		//2
+//#define ADC_PRESCALER 2		//4
+//#define ADC_PRESCALER 3		//8
+//#define ADC_PRESCALER 4		//16
+//#define ADC_PRESCALER 5		//32
+//#define ADC_PRESCALER 6		//64
+#define ADC_PRESCALER 7		//128
 
 
 #endif /* ADC_M328PB_H_ */
