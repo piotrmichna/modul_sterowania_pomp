5b75a33ce03f2674dd286e59c6053689d0438d90
Piotr Michna <pm@piotrmichna.pl>
2020-08-24 23:19:30 +0200

    utworzenie plikow biblioteki ADC

---
+++ mod/adc_m328pb.c
@@ -0,0 +1,13 @@
+/*
+ * adc_m328pb.c
+ *
+ * Created: 24.08.2020 23:17:46
+ * Author : Piotr Michna
+ * e-mail : pm@piotrmichna.pl
+ */ 
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+
+#include "adc_m328pb.h"
---
+++ mod/adc_m328pb.h
@@ -0,0 +1,16 @@
+/*
+ * adc_m328pb.h
+ *
+ * Created: 24.08.2020 23:16:24
+  * Author : Piotr Michna
+  * e-mail : pm@piotrmichna.pl
+ */ 
+
+
+#ifndef ADC_M328PB_H_
+#define ADC_M328PB_H_
+
+
+
+
+#endif /* ADC_M328PB_H_ */
